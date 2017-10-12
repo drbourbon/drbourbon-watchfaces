207012604f965ae38bf8910e224d14177ac69fc1
Fabio Barbon <fabio.barbon@gmail.com>
2017-10-12 16:01:49 +0200

    export

---
+++ app/src/main/java/it/drbourbon/mywatchfaces/HugeDigitsWatchFaceSplt.java
@@ -0,0 +1,15 @@
+package it.drbourbon.mywatchfaces;
+
+import es.malvarez.mywatchfaces.AbstractWatchFaceSlpt;
+import it.drbourbon.mywatchfaces.widget.FuzzyTextClock;
+import it.drbourbon.mywatchfaces.widget.HugeDigitClock;
+
+/**
+ * Created by fabio on 21/05/17.
+ */
+
+public class HugeDigitsWatchFaceSplt extends AbstractWatchFaceSlpt {
+    public HugeDigitsWatchFaceSplt() {
+        super(new HugeDigitClock());
+    }
+}
